c0b0689093340a099178d0bb3fc440a7cdea0f4e
吕红超 <3128134930@qq.com>
2023-05-23 17:34:36 +0800

    捷电网盘上传

--- src/components/multiple/index.vue
+++ src/components/multiple/index.vue
@@ -1,38 +1,74 @@
 <template>
-  <div>
-    <el-upload 
-        class="upload-demo"
-        ref="upload"
-        accept=".txt, .txts, .pdf, .docx , .jpg, .png"
-        :multiple="true"
-        :action="uploadFileUrl"
-        :headers="headers"
-        :on-change="handleFileChange"
-        :on-remove="onRemove"
-        :before-remove="beforeRemove"
-        :on-exceed="fileExceed"
-        :auto-upload="false"
-        :file-list="fileList"
-        >
-        <el-button slot="trigger" size="small" type="primary">选取附件</el-button>
-        <el-button style="margin-left: 10px;" v-if="fileList.length>0" size="small" type="success" @click="submitFileForm">上传附件</el-button>
+  <div class="upload-file">
+    <el-upload
+      :action="uploadFileUrl"
+      :before-upload="handleBeforeUpload"
+      :file-list="fileList"
+      :on-error="handleUploadError"
+      :on-exceed="handleExceed"
+      :on-success="handleUploadSuccess"
+      :show-file-list="false"
+      :headers="headers"
+      class="upload-file-uploader"
+      ref="upload"
+    >
+      <!-- 上传按钮 -->
+      <el-button size="mini" type="primary">选取文件</el-button>
+      <!-- 上传提示 -->
+      <div class="el-upload__tip" slot="tip" v-if="showTip">
+        请上传
+        <template v-if="fileSize"> 大小不超过 <b style="color: #f56c6c">{{ fileSize }}MB</b> </template>
+        <template v-if="fileType"> 格式为 <b style="color: #f56c6c">{{ fileType.join("/") }}</b> </template>
+        的文件
+      </div>
     </el-upload>
+
+    <!-- 文件列表 -->
+    <transition-group class="upload-file-list el-upload-list el-upload-list--text" name="el-fade-in-linear" tag="ul">
+      <li :key="file.uid" class="el-upload-list__item ele-upload-list__item-content" v-for="(file, index) in list">
+        <el-link :href="file.response.data.url" :underline="false" target="_blank">
+          <span class="el-icon-document"> {{ file.response.data.name }} </span>
+        </el-link>
+        <div class="ele-upload-list__item-content-action">
+          <el-link :underline="false" @click="handleDelete(index)" type="danger">删除</el-link>
+        </div>
+      </li>
+    </transition-group>
   </div>
 </template>
 
 <script>
 import { getToken } from "@/utils/auth";
+
 export default {
+  props: {
+    // 值
+    value: [String, Object, Array],
+    // 大小限制(MB)
+    fileSize: {
+      type: Number,
+      default: 5,
+    },
+    // 文件类型, 例如['png', 'jpg', 'jpeg']
+    fileType: {
+      type: Array,
+      // <!--"doc", "xls", "ppt", "txt", "pdf",-->
+      default: () => ["avi","rmvb","mp4","jpg","jpeg","png","gif"],
+    },
+    // 是否显示提示
+    isShowTip: {
+      type: Boolean,
+      default: true
+    }
+  },
   data() {
     return {
-       //回显附件列表
-      fileList: [],
-      //上传附件列表
-      files:[],
-      uploadFileUrl: process.env.VUE_APP_BASE_API + "/file/upload",
+      uploadFileUrl: process.env.VUE_APP_BASE_API + "/file/upload", // 上传的图片服务器地址
       headers: {
         Authorization: "Bearer " + getToken(),
       },
+      fileList: [],
+      lists: [],
     };
   },
   computed: {
@@ -44,67 +80,70 @@ export default {
     list() {
       let temp = 1;
       if (this.value) {
-        // 首先将值转为数组
-        const list = Array.isArray(this.value) ? this.value : [this.value];
-        // 然后将数组转为对象数组
-        return list.map((item) => {
-          if (typeof item === "string") {
-            item = { name: item, url: item };
-          }
-          item.uid = item.uid || new Date().getTime() + temp++;
-          return item;
-        });
+        return this.lists
       } else {
         this.fileList = [];
         return [];
       }
     },
   },
-  mounted(){
-     this.getFiles()
-  },
   methods: {
-   //by文章id获取附件列表
-    getFiles(){
-      console.log('回显')
+    // 上传前校检格式和大小
+    handleBeforeUpload(file) {
+      // 校检文件类型
+      if (this.fileType) {
+        let fileExtension = "";
+        if (file.name.lastIndexOf(".") > -1) {
+          fileExtension = file.name.slice(file.name.lastIndexOf(".") + 1);
+        }
+        const isTypeOk = this.fileType.some((type) => {
+          if (file.type.indexOf(type) > -1) return true;
+          if (fileExtension && fileExtension.indexOf(type) > -1) return true;
+          return false;
+        });
+        if (!isTypeOk) {
+          this.$message.error(`文件格式不正确, 请上传${this.fileType.join("/")}格式文件!`);
+          return false;
+        }
+      }
+      // 校检文件大小
+      if (this.fileSize) {
+        const isLt = file.size / 1024 / 1024 < this.fileSize;
+        if (!isLt) {
+          this.$message.error(`上传文件大小不能超过 ${this.fileSize} MB!`);
+          return false;
+        }
+      }
+      return true;
     },
-    //上传文件之前
-    beforeUpload(file){
-    },
-     // 上传发生变化钩子
-    handleFileChange(file, fileList){
-      this.files = fileList;
-      this.fileList = fileList
-      // if(fileList.findIndex(f=>f.name===file.name)!=fileList.findLastIndex(f=>f.name===file.name))
-      // {
-      //   ElMessage.info(file.name +" 文件已存在")
-      //   fileList.pop()
-      // }
-    },
-    //文件个数超过最大限制时
-    fileExceed(file, fileList){
+    // 文件个数超出
+    handleExceed(file, fileList){
       if(this.fileList.length>10){
          this.$message.warning("附件个数不能超过10个")
       }
     },
-    //删除前的钩子
-     beforeRemove(file, fileList) {
-        return this.$confirm(`确定移除 ${ file.name }？`);
-      },
-      //删除的钩子
-    onRemove(file,fileList){
-      this.fileList = fileList
+    // 上传失败
+    handleUploadError(err) {
+      this.$message.error("上传失败, 请重试");
     },
-    // 提交上传文件
-    submitFileForm(file,fileList){
-      console.log(this.fileList,"this.fileListthis.fileList")
-       if(this.fileList.length === 0 ) {
-            this.$message.warning('未选择文件!')
-        }else{
-            this.$refs.upload.submit()
-        }
-    }
-  }
+    // 上传成功回调
+    handleUploadSuccess(res, file, fileList) {
+      this.$message.success("上传成功");
+      this.lists = fileList
+      var obj = fileList.map((item,index)=>{
+        return item.response.data.url
+      })
+      this.$emit("input", obj.toString());
+    },
+    // 删除文件
+    handleDelete(index) {
+      this.fileList.splice(index, 1);
+      this.$emit("input", '');
+    },
+  },
+  created() {
+    this.fileList = this.list;
+  },
 };
 </script>
 
@@ -128,5 +167,3 @@ export default {
   margin-right: 10px;
 }
 </style>
-
-
